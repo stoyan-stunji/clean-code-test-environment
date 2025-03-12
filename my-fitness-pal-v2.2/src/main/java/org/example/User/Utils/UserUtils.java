5ed582e7a3a88527441a180f91f624577b953923
Stoyan Ivanov <115835362+stoyan-stunji@users.noreply.github.com>
2025-03-12 21:13:11 +0200

    Delete my-fitness-pal-v2.2 directory

--- my-fitness-pal-v2.2/src/main/java/org/example/User/Utils/UserUtils.java
+++
@@ -1,9 +0,0 @@
-package org.example.User.Utils;
-
-import org.example.User.User;
-
-public interface UserUtils
-{
-    User getUserByUsername(String username);
-    boolean createUser(String username, String password);
-}
--- my-fitness-pal-v2.2/src/test/java/UserTests/UserBankTests.java
+++
@@ -1,5 +0,0 @@
-package UserTests;
-
-public class UserBankTests {
-    // I don't know how to make the tests work!!
-}
